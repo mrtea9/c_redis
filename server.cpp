396a0c8bbfc4f4756edfff36db58880c549b66da
mrtea9 <ionmeriacri111@gmail.com>
2024-10-22 15:17:19 +0300

    Protocol Parsing

--- server.cpp
+++ server.cpp
@@ -8,6 +8,9 @@
 #include <netinet/ip.h>
 #include <stdbool.h>
 #include <sys/socket.h>
+#include <assert.h>
+
+const size_t k_max_msg = 4096;
 
 static void msg(const char* msg) {
 	fprintf(stderr, "%s\n", msg);
@@ -19,17 +22,68 @@ static void die(const char* msg) {
 	abort();
 }
 
-static void do_something(int connfd) {
-	char rbuf[64] = {};
-	ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-	if (n < 0) {
-		msg("read() error");
-		return;
+static int32_t read_full(int fd, char* buf, size_t n) {
+	while (n > 0) {
+		ssize_t rv = read(fd, buf, n);
+		if (rv <= 0) {
+			return -1;
+		}
+		assert((size_t)rv <= n);
+		n -= (size_t)rv;
+		buf += rv;
 	}
-	printf("client says: %s\n", rbuf);
+	return 0;
+}
 
-	char wbuf[] = "world";
-	write(connfd, wbuf, strlen(wbuf));
+static int32_t write_all(int fd, char* buf, size_t n) {
+	while (n > 0) {
+		ssize_t rv = write(fd, buf, n);
+		if (rv <= 0) {
+			return -1;
+		}
+		assert((size_t)rv <= n);
+		n -= (size_t)rv;
+		buf += rv;
+	}
+	return 0;
+}
+
+static int32_t one_request(int connfd) {
+	char rbuf[4 + k_max_msg + 1];
+	errno = 0;
+	int32_t err = read_full(connfd, rbuf, 4);
+	if (err) {
+		if (errno == 0) {
+			msg("EOF");
+		}
+		else {
+			msg("read() error");
+		}
+		return err;
+	}
+
+	uint32_t len = 0;
+	memcpy(&len, rbuf, 4);
+	if (len > k_max_msg) {
+		msg("too long");
+		return -1;
+	}
+
+	err = read_full(connfd, &rbuf[4], len);
+	if (err) {
+		msg("read() error");
+		return err;
+	}
+
+	rbuf[4 + len] = '\0';
+	printf("client says: %s\n", &rbuf[4]);
+
+	const char reply[] = "world";
+	char wbuf[4 + sizeof(reply)];
+	len = (uint32_t)strlen(reply);
+	memcpy(wbuf, &len, 4);
+	memcpy(&wbuf[4], reply, len);
+	return write_all(connfd, wbuf, 4 + len);
 }
 
 int main() {
@@ -55,13 +109,19 @@ int main() {
 
 	while (true) {
 		struct sockaddr_in client_addr = {};
-		socklen_t addrlen = sizeof(client_addr);
-		int connfd = accept(fd, (struct sockaddr*)&client_addr, &addrlen);
+		socklen_t socklen = sizeof(client_addr);
+		int connfd = accept(fd, (struct sockaddr*)&client_addr, &socklen);
 		if (connfd < 0) {
 			continue;
 		}
 
-		do_something(connfd);
+		while (true) {
+			int32_t err = one_request(connfd);
+			if (err) {
+				break;
+			}
+		}
+
 		close(connfd);
 	}
 
